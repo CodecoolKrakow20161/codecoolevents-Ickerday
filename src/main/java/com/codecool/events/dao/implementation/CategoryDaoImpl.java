27cb1804e3da37d949728a0bbc26a806135b6906
Ickerday <elitarform@gmail.com>
2017-05-04 18:30:28 +0200

    Add example DAO implementation

---
+++ src/main/java/com/codecool/events/dao/implementation/CategoryDaoImpl.java
@@ -0,0 +1,18 @@
+package com.codecool.events.dao.implementation;
+
+import com.codecool.events.dao.CategoryDao;
+import com.codecool.events.model.Category;
+import java.util.List;
+
+public class CategoryDaoImpl implements CategoryDao {
+
+  @Override
+  public Category find(Integer id) {
+    return null;
+  }
+
+  @Override
+  public List<Category> getAllCategories() {
+    return null;
+  }
+}
---
+++ src/main/java/com/codecool/events/dao/implementation/EventDaoImpl.java
@@ -0,0 +1,18 @@
+package com.codecool.events.dao.implementation;
+
+import com.codecool.events.dao.EventDao;
+import com.codecool.events.model.Event;
+import java.util.List;
+
+public class EventDaoImpl implements EventDao {
+
+  @Override
+  public Event find(Integer id) {
+    return null;
+  }
+
+  @Override
+  public List<Event> getAllEvents() {
+    return null;
+  }
+}
