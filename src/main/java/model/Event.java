1db0b1fc888b9defa3f4fb0b4c6d39d144ea8896
Ickerday <elitarform@gmail.com>
2017-05-04 14:37:34 +0200

    Add Event model

---
+++ src/main/java/model/Event.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.util.Date;
+
+public class Event extends BaseModel {
+
+  private Category category;
+  private Date date;
+
+
+  public Event(Integer id, String name, String description, Category category,
+      Date date) {
+    super(id, name, description);
+    this.category = category;
+    this.date = date;
+  }
+}
