d1fa476df8a12f2b714578898559ea81384734b6
Ickerday <elitarform@gmail.com>
2017-05-04 14:37:07 +0200

    Add BaseModel

---
+++ src/main/java/model/BaseModel.java
@@ -0,0 +1,26 @@
+package model;
+
+class BaseModel {
+
+  private Integer id;
+  private String name;
+  private String description;
+
+  BaseModel(Integer id, String name, String description) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+}
