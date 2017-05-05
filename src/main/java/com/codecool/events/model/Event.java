61178a9beab7034cf98b88f8fa97b74bf419dbfb
Ickerday <elitarform@gmail.com>
2017-05-05 20:27:21 +0200

    Add additional Event constructor used when adding new Event

--- src/main/java/com/codecool/events/model/Event.java
+++ src/main/java/com/codecool/events/model/Event.java
@@ -7,8 +7,13 @@ public class Event extends BaseModel {
   private Category category;
   private Date date;
 
-  public Event(Integer id, String name, String description, Category category,
-      Date date) {
+  public Event(String name, String description, Category category, Date date) {
+    super(name, description);
+    this.category = category;
+    this.date = date;
+  }
+
+  public Event(Integer id, String name, String description, Category category, Date date) {
     super(id, name, description);
     this.category = category;
     this.date = date;
