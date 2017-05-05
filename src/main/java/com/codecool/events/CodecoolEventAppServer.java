21fc067076b118c0b51eba0e5f568ca696da6a98
Ickerday <elitarform@gmail.com>
2017-05-05 16:42:11 +0200

    CodecoolEventAppServer now will try to display events from DB

--- src/main/java/com/codecool/events/CodecoolEventAppServer.java
+++ src/main/java/com/codecool/events/CodecoolEventAppServer.java
@@ -2,11 +2,22 @@ package com.codecool.events;
 
 import static spark.Spark.*;
 
+import com.codecool.events.controller.EventController;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 class CodecoolEventAppServer {
 
   CodecoolEventAppServer() {
     port(8888);
+    staticFileLocation("/public");
 
-    get("/", (req, res) -> "helo mi!");
+    get("/shrug", (req, res) -> "¯\\_(ツ)_/¯");
+
+    try {
+      get("/", EventController::renderEvents, new ThymeleafTemplateEngine());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
   }
 }
