d80bb73d7fec4044129e071726f2015582126ab1
Ickerday <elitarform@gmail.com>
2017-05-05 19:01:58 +0200

    EventController now returns correct ModelAndView object

--- src/main/java/com/codecool/events/controller/EventController.java
+++ src/main/java/com/codecool/events/controller/EventController.java
@@ -1,18 +1,22 @@
 package com.codecool.events.controller;
 
+import com.codecool.events.dao.implementation.EventDaoImpl;
+import com.codecool.events.model.Event;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import spark.ModelAndView;
 import spark.Request;
 import spark.Response;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class EventController {
-    public static ModelAndView renderProducts(Request req, Response res) {
-        //Get events from database by Dao
 
-        Map params = new HashMap<>();
-        params.put("eventContainer", "Codecool cinema");
-        return new ModelAndView(params, "product/index");
-    }
+  public static ModelAndView renderEvents(Request req, Response res) {
+    EventDaoImpl eventDao = new EventDaoImpl();
+    Map<String, List<Event>> params = new HashMap<>();
+
+    params.put("eventList", eventDao.getAllEvents());
+
+    return new ModelAndView(params, "product/index");
+  }
 }
