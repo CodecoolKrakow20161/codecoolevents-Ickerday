ef13ccb93b1564d04286c2349efc5e6f44debc88
Ickerday <elitarform@gmail.com>
2017-05-05 20:28:29 +0200

    Add untested handleAddEditRequest

--- src/main/java/com/codecool/events/controller/EventController.java
+++ src/main/java/com/codecool/events/controller/EventController.java
@@ -1,7 +1,10 @@
 package com.codecool.events.controller;
 
+import com.codecool.events.dao.implementation.CategoryDaoImpl;
 import com.codecool.events.dao.implementation.EventDaoImpl;
+import com.codecool.events.model.Category;
 import com.codecool.events.model.Event;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,5 +21,21 @@ public class EventController {
     params.put("eventList", eventDao.getAllEvents());
 
     return new ModelAndView(params, "product/index");
+  }
+
+  public static Object handleAddEditRequest(Request req, Response res) {
+    CategoryDaoImpl categoryDao = new CategoryDaoImpl();
+    EventDaoImpl eventDao = new EventDaoImpl();
+
+    String eventName = req.queryParams("event-name");
+    String eventDescription = req.queryParams("event-description");
+    Category eventCategory = categoryDao
+        .find(Integer.getInteger(req.queryParams("event-category-id")));
+
+    eventDao.upsert(new Event(eventName, eventDescription, eventCategory, new Date()));
+
+    res.status(201);
+    res.redirect("/");
+    return "";
   }
 }
