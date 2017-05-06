7a86a6565c1b9e47be6be7707481d0250b545197
Ickerday <elitarform@gmail.com>
2017-05-06 15:38:32 +0200

    Add getCategoryBy, getEventBy to DbConnector

--- src/main/java/com/codecool/events/dao/implementation/DbConnector.java
+++ src/main/java/com/codecool/events/dao/implementation/DbConnector.java
@@ -1,5 +1,6 @@
 package com.codecool.events.dao.implementation;
 
+import com.codecool.events.model.Category;
 import com.codecool.events.model.Event;
 
 import java.sql.*;
@@ -23,32 +24,106 @@ class DbConnector {
     conn.close();
   }
 
-  List<Event> getEvents() throws SQLException {
-    List<Event> eventList = new ArrayList<>();
-    CategoryDaoImpl categoryDao = new CategoryDaoImpl();
+  Event getEventBy(Integer id) throws SQLException {
+    Event foundEvent = null;
 
     try {
       Connection conn = connect();
       Statement dbStatement = conn.createStatement();
 
-      ResultSet dbQuery = dbStatement.executeQuery("SELECT * FROM Events");
+      ResultSet dbQuery = dbStatement.executeQuery("SELECT * FROM Event WHERE id=" + id);
+
+      if (dbQuery.next()) {
+        foundEvent = new Event(
+            dbQuery.getInt("id"),
+            dbQuery.getString("name"),
+            dbQuery.getString("description"),
+            getCategoryBy(dbQuery.getInt("categoryID")),
+            new Date(Long.parseLong(dbQuery.getString("Date")))
+        );
+      }
+    } catch (Exception e) {
+      throw new SQLException("Couldn't found event!");
+    }
+    return foundEvent;
+  }
+
+  Category getCategoryBy(Integer id) throws SQLException {
+    Category foundCategory = null;
+
+    try {
+      Connection conn = connect();
+      Statement dbStatement = conn.createStatement();
+
+      ResultSet dbQuery = dbStatement.executeQuery("SELECT * FROM Category WHERE id=" + id);
+
+      if (dbQuery.next()) {
+        foundCategory = new Category(
+            dbQuery.getInt("id"),
+            dbQuery.getString("name"),
+            dbQuery.getString("description")
+        );
+      }
+    } catch (Exception e) {
+      throw new SQLException("Couldn't found category!");
+    }
+    return foundCategory;
+  }
+
+  List<Event> getEvents() throws SQLException {
+    List<Event> eventList = new ArrayList<>();
+
+    try {
+      Connection conn = connect();
+      Statement dbStatement = conn.createStatement();
+
+      ResultSet dbQuery = dbStatement.executeQuery("SELECT * FROM Event");
 
       while (dbQuery.next()) {
         eventList.add(new Event(
             dbQuery.getInt("id"),
             dbQuery.getString("name"),
             dbQuery.getString("description"),
-            categoryDao.find(dbQuery.getInt("categoryID")),
+            getCategoryBy(dbQuery.getInt("categoryID")),
             new Date(Long.parseLong(dbQuery.getString("Date")))
         ));
       }
 
+      dbQuery.close();
+      dbStatement.close();
+      closeConnection(conn);
     } catch (SQLException e) {
       e.printStackTrace();
       throw new SQLException("SQL ERROR: Couldn't get events!");
     }
-
     return eventList;
+  }
+
+  List<Category> getCategories() throws SQLException {
+    List<Category> categoryList = new ArrayList<>();
+
+    try {
+      Connection conn = connect();
+      Statement dbStatement = conn.createStatement();
+
+      ResultSet dbQuery = dbStatement.executeQuery("SELECT * FROM Category");
+
+      while (dbQuery.next()) {
+        categoryList.add(new Category(
+            dbQuery.getInt("id"),
+            dbQuery.getString("name"),
+            dbQuery.getString("description")
+        ));
+
+        dbQuery.close();
+        dbStatement.close();
+        closeConnection(conn);
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+      throw new SQLException("SQL ERROR: Couldn't get categories!");
+    }
+    return categoryList;
   }
 
   void upsertEvent(Event event) throws SQLException {
@@ -56,8 +131,8 @@ class DbConnector {
       Connection conn = connect();
       Statement dbStatement = conn.createStatement();
 
-      dbStatement.execute("INSERT OR REPLACE INTO Events (id, name, description, date, categoryID)"
-          + " VALUES ((SELECT id FROM Events WHERE id=" +
+      dbStatement.execute("INSERT OR REPLACE INTO Event (id, name, description, date, categoryID)"
+          + " VALUES ((SELECT id FROM Event WHERE id=" +
           event.getId() + "), '" +
           event.getName() + "', '" +
           event.getDescription() + "', " +
@@ -66,8 +141,6 @@ class DbConnector {
 
       dbStatement.close();
       closeConnection(conn);
-
-
     } catch (Exception e) {
       e.printStackTrace();
       throw new SQLException("SQL ERROR: Couldn't upsert to database!");
@@ -79,7 +152,7 @@ class DbConnector {
       Connection conn = connect();
       Statement dbStatement = conn.createStatement();
 
-      String query = "DELETE FROM Events WHERE id=" + event.getId();
+      String query = "DELETE FROM Event WHERE id=" + event.getId();
 
       dbStatement.execute(query);
       dbStatement.close();
