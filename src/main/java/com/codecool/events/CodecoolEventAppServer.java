60ff8ab2c8ffdb648b9b424daf973599f3784db2
Ickerday <elitarform@gmail.com>
2017-05-06 20:34:28 +0200

    Add correct routes for event manipulation to server

--- src/main/java/com/codecool/events/CodecoolEventAppServer.java
+++ src/main/java/com/codecool/events/CodecoolEventAppServer.java
@@ -8,15 +8,22 @@ import spark.template.thymeleaf.ThymeleafTemplateEngine;
 class CodecoolEventAppServer {
 
   CodecoolEventAppServer() {
+
     port(8888);
     staticFileLocation("/static");
 
-    get("/shrug", (req, res) -> "¯\\_(ツ)_/¯");
+    // TODO
+    get("/event/:id/edit", EventController::renderEvents, new ThymeleafTemplateEngine());
+
+    //TODO
+    get("/event/:id/delete", EventController::renderEvents, new ThymeleafTemplateEngine());
+
+    // TODO
+    get("/category/:id", EventController::renderEvents, new ThymeleafTemplateEngine());
+
+    // TODO
+    post("/event/add", EventController::handleAddEditRequest);
 
     get("/", EventController::renderEvents, new ThymeleafTemplateEngine());
-
-    post("/article/create", EventController::handleAddEditRequest);
-
-
   }
 }
