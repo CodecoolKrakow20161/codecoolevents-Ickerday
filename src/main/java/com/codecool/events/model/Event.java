3fd8cf1846e9aa578e5b6a9293a0bba2ee62a7dd
Ickerday <elitarform@gmail.com>
2017-05-07 18:57:56 +0200

    Add link attribute to constructor, generate toString

--- src/main/java/com/codecool/events/model/Event.java
+++ src/main/java/com/codecool/events/model/Event.java
@@ -8,10 +8,11 @@ public class Event extends BaseModel {
   private Date date;
   private String link;
 
-  public Event(String name, String description, Category category, Date date) {
+  public Event(String name, String description, Category category, Date date, String link) {
     super(name, description);
     this.category = category;
     this.date = date;
+    this.link = link;
   }
 
   public Event(Integer id, String name, String description, Category category, Date date, String link) {
@@ -31,5 +32,14 @@ public class Event extends BaseModel {
 
   public Date getDate() {
     return date;
+  }
+
+  @Override
+  public String toString() {
+    return super.toString() + " Event{" +
+        "category=" + category +
+        ", date=" + date +
+        ", link='" + link + '\'' +
+        '}';
   }
 }
