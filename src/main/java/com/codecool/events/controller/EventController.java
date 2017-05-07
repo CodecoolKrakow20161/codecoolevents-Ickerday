9c0759b2c6e0033bbbbcd5b6f73054bb801bc6d2
Ickerday <elitarform@gmail.com>
2017-05-07 19:34:09 +0200

    Add renderEditForm(), handleDeleteRequest() to EventController

--- src/main/java/com/codecool/events/controller/EventController.java
+++ src/main/java/com/codecool/events/controller/EventController.java
@@ -53,9 +53,22 @@ public class EventController {
     return new ModelAndView(params, "event/add");
   }
 
+  // TODO
+  public static ModelAndView renderEditForm(Request req, Response res) {
+    CategoryDao categoryDao = new CategoryDaoImpl();
+    Map<String, Object> params = new HashMap<>();
+
+    try {
+      params.put("categoryList", categoryDao.getAllCategories());
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+    return new ModelAndView(params, "event/add");
+  }
+
   public static ModelAndView handleAddEditRequest(Request req, Response res) {
-    CategoryDaoImpl categoryDao = new CategoryDaoImpl();
-    EventDaoImpl eventDao = new EventDaoImpl();
+    CategoryDao categoryDao = new CategoryDaoImpl();
+    EventDao eventDao = new EventDaoImpl();
 
     try {
       String eventName = req.queryParams("event-name");
@@ -69,6 +82,27 @@ public class EventController {
     } catch (SQLException e) {
       e.printStackTrace();
     }
+
+    res.status(201);
+    res.redirect("/");
+    return renderEvents(req, res);
+  }
+
+  public static ModelAndView handleDeleteRequest(Request req, Response res) {
+    EventDao eventDao = new EventDaoImpl();
+
+    try {
+      if (req.params().containsKey(":id")) {
+        Integer eventID = Integer.parseInt(req.params(":id"));
+
+        eventDao.delete(eventDao.find(eventID));
+      }
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    res.status(201);
+    res.redirect("/");
     return renderEvents(req, res);
   }
 }
